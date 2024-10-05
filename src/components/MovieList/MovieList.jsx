10b2f422538ec54e553b1c046aecedcaf20b75a5
ashechek <ashechek3@gmail.com>
2024-10-05 20:06:29 +0300

    addUseLocation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import s from "./MovieList.module.css";
 
 const MovieList = ({ list, state }) => {
+  const location = useLocation();
   return (
     <>
       <ul className={s.movieList}>
         {list.map(({ title, id, poster_path }) => (
-          <Link to={`/movies/${id}`} key={id} state={state}>
+          <Link to={`/movies/${id}`} key={id} state={location}>
             <li className={s.movieItem}>
               <h4>{title}</h4>
             </li>
